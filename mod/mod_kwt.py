e109b6c4dfce4278f8b343d23c143ef1deca3daf
ommicom <ororshii@gmail.com>
2013-06-02 21:49:04 +0700

    mod_kwt

--- mod/mod_kwt.py
+++ mod/mod_kwt.py
@@ -1,6 +1,15 @@
 __author__ = 'ommicom'
 
+import os
+import glob
+
 def run(files, mod_result=None):
     sett = __import__('set_kwt')
-    
+    try:
+        os.chdir(sett.source_dir)
+        for kwt in glob.glob(sett.mask):
+            print(kwt)
+    except EnvironmentError:
+        pass
+
     return files, mod_result

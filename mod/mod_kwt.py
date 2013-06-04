da001c42c322febb7dce5b811959dee039225d7d
ommicom <oroshii@gmail.com>
2013-06-04 20:05:56 +0700

    create sqlite reestr

--- mod/mod_kwt.py
+++ mod/mod_kwt.py
@@ -7,9 +7,21 @@ import datetime
 sett = __import__('set_kwt')
 
 def db_reestr(inf_row):
-    pass
+    db_name = sett.db.get('name', os.path.join(os.getcwd(),'kwt.sqlite'))
+    con = sqlite3.connect(db_name, check_same_thread=False)
+    try:
+        cur = con.cursor()
+        # for row in inf_row:
+        #     cur.execute('INSERT INTO reestr (kwt, mes, kwt_res, kwt_dt, kwt_tm) VALUES (?, ?, ?, ?, ?)', row)
+        #     cur.execute()
+        cur.executemany('INSERT INTO reestr (kwt, mes, kwt_res, kwt_dt, kwt_tm) VALUES (?, ?, ?, ?, ?)', inf_row)
+        con.commit()
+    except sqlite3.OperationalError as err:
+        print(err)
+    finally:
+        con.close()
     # print(inf_row)
-    # db_name = sett.db.get('name', os.path.join(os.getcwd(),'kwt.sqlite'))
+
     # con = sqlite3.connect(db_name, check_same_thread=False)
     # try:
     #     cur = con.cursor()
@@ -43,17 +55,16 @@ def run(files, mod_result=None):
             inf_row.append(subpart[0])
             inf_row.append(subpart[1])
             inf_row.append(subpart[2])
-
         res_row.append(inf_row)
         os.rename(kwt, ''.join((post_dir, '~'.join(('',os.path.basename(kwt))))))
 
-    dt = 'reestr_'+datetime.datetime.today().strftime('%Y%m%d%H%M%S')+'.txt'
-    with open(os.path.join(reestr_dir, dt), 'w') as f:
-        for res in res_row:
-            f.write('\t'.join(res))
-            f.write('\n')
-    if db_enable:
-        db_reestr(res_row)
-
+    if res_row:
+        dt = 'reestr_'+datetime.datetime.today().strftime('%Y%m%d%H%M%S')+'.txt'
+        with open(os.path.join(reestr_dir, dt), 'w') as f:
+            for res in res_row:
+                f.write('\t'.join(res))
+                f.write('\n')
+        if db_enable:
+            db_reestr(res_row)
 
     return files, mod_result
